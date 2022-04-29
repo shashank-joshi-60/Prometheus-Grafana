cec68a77dcb918310870cd31c660105646f888b4
paresh0410 <paresh@mobeserv.com>
2022-04-29 12:50:57 +0530

    change

--- install-grafana.sh
+++ install-grafana.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -y adduser libfontconfig1
-wget https://dl.grafana.com/oss/release/grafana_8.5.1_amd64.deb
+sudo wget https://dl.grafana.com/oss/release/grafana_8.5.1_amd64.deb
 sudo dpkg -i grafana_8.5.1_amd64.deb
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
